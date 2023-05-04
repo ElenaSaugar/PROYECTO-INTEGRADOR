4de7644e03cf6962a0a785463b7b6ac803a3f0f4
DavidOS <118904142+DavidOS10@users.noreply.github.com>
2023-05-05 00:07:51 +0200

    Delete Programacion/Programacion/src/mvc/Vista directory

--- Programacion/Programacion/src/mvc/Vista/InterfazProyecto.java
+++
@@ -1,5 +0,0 @@
-package mvc.Vista;
-
-public interface InterfazProyecto {
-
-}
--- Programacion/Programacion/src/mvc/Vista/VentanaProyecto.java
+++
@@ -1,7 +0,0 @@
-package mvc.Vista;
-
-import javax.swing.JFrame;
-
-public class VentanaProyecto extends JFrame implements InterfazProyecto{
-
-}
