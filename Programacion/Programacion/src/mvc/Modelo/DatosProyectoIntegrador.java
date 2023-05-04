498319d687d943d5e489e74109deb66963e4b740
DavidOS <118904142+DavidOS10@users.noreply.github.com>
2023-05-05 00:09:20 +0200

    Add files via upload

---
+++ Programacion/Programacion/src/mvc/Modelo/DatosProyectoIntegrador.java
@@ -0,0 +1,70 @@
+package mvc.Modelo;
+
+public class DatosProyectoIntegrador {
+
+	private String nombreProy;
+	private String asignatura;
+	private String componentes;
+	private String grupo;
+	private int codProy;
+	private double notaFinal;
+	private String url;
+	private int año;
+	private String curso;
+	
+	public String getNombreProy() {
+		return nombreProy;
+	}
+	public void setNombreProy(String nombreProy) {
+		this.nombreProy = nombreProy;
+	}
+	public String getAsignatura() {
+		return asignatura;
+	}
+	public void setAsignatura(String asignatura) {
+		this.asignatura = asignatura;
+	}
+	public String getComponentes() {
+		return componentes;
+	}
+	public void setComponentes(String componentes) {
+		this.componentes = componentes;
+	}
+	public String getGrupo() {
+		return grupo;
+	}
+	public void setGrupo(String grupo) {
+		this.grupo = grupo;
+	}
+	public int getCodProy() {
+		return codProy;
+	}
+	public void setCodProy(int codProy) {
+		this.codProy = codProy;
+	}
+	public double getNotaFinal() {
+		return notaFinal;
+	}
+	public void setNotaFinal(double notaFinal) {
+		this.notaFinal = notaFinal;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public int getAño() {
+		return año;
+	}
+	public void setAño(int año) {
+		this.año = año;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	
+}
