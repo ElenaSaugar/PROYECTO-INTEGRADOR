959ec6d6bed3da2261d94627c8608972adb7d151
DavidOS <118904142+DavidOS10@users.noreply.github.com>
2023-05-07 23:56:13 +0200

    Update InicioSesion.java

--- Programacion/Programacion/src/mvc/Vista/InicioSesion.java
+++ Programacion/Programacion/src/mvc/Vista/InicioSesion.java
@@ -7,8 +7,11 @@ public class InicioSesion extends JFrame {
 
 	private JPanel panel;
 	private JComboBox<String> alumProfesorCombo;
-	private JPasswordField passwordField;
+	private JPasswordField contrasena;
 	private JButton loginButton;
+	private JButton vistaContrasena;
+	private boolean vista;
+	private char echoChar;
 
 	    public InicioSesion() {
 	        // Configuración de la ventana principal
@@ -39,15 +42,34 @@ public class InicioSesion extends JFrame {
 	        passwordLabel.setBounds(24, 73, 138, 25);
 	        panel.add(passwordLabel);
 
-	        // Creación del campo de contraseña
-	        passwordField = new JPasswordField(20);
-	        passwordField.setBounds(174, 73, 155, 25);
-	        panel.add(passwordField);
-
-	        // Creación del botón de inicio de sesión
+	     // Creación del botón de inicio de sesión
 	        loginButton = new JButton("Iniciar sesión");
 	        loginButton.setBounds(24, 108, 335, 30);
 	        panel.add(loginButton);
+	        
+	        // Creación del campo de contraseña
+	        contrasena = new JPasswordField(20);
+	        contrasena.setBounds(174, 73, 155, 25);
+	        panel.add(contrasena);
+	        
+	        vista = false;
+			 echoChar = contrasena.getEchoChar();
+			
+			vistaContrasena = new JButton ("Ver");
+			vistaContrasena.addActionListener(new ActionListener(){
+				public void actionPerformed (ActionEvent e) {
+					if(!vista) {
+						vista = true;
+						contrasena.setEchoChar((char)0);
+					}
+					else {
+						vista = false;
+						contrasena.setEchoChar(echoChar);
+					}
+				}});
+			
+			vistaContrasena.setBounds(339, 75, 25, 20);
+			panel.add(vistaContrasena);
 
 	        // Agregar el panel al contenedor principal
 	        setContentPane(panel);
