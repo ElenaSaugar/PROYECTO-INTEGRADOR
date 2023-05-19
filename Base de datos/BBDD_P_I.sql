cc05e78540255d3f7d9023956ce4c175a7bbdb52
Celia Rubio Pais <118897737+rubiocelia@users.noreply.github.com>
2023-05-19 21:39:36 +0200

    Add files via upload
    base de datos actualizada

--- Base de datos/BBDD_P_I.sql
+++ Base de datos/BBDD_P_I.sql
@@ -5,22 +5,18 @@ DROP TABLE IF EXISTS Area;
 DROP TABLE IF EXISTS Profesores;
 
 CREATE TABLE Area (
-  CodAr INT PRIMARY KEY,
-  Nombre VARCHAR(50),
+  CodAr VARCHAR(10) PRIMARY KEY,
   Descripcion VARCHAR(100)
 );
 
 CREATE TABLE Proyecto_Integrador (
-  CodProy INT PRIMARY KEY,
-  nombreProy VARCHAR(50),
-  asignatura VARCHAR(50),
-  componentes VARCHAR(150),
-  grupo VARCHAR(10),
-  notaFinal DECIMAL(4,2),
-  URL VARCHAR(100),
-  año INT,
-  curso VARCHAR(10),
-  Area_CodAr INT,
+  CodProy INT auto_increment primary KEY ,
+  nombreProy VARCHAR(50) not null,
+  notaFinal DECIMAL(4,2) not null,
+  URL VARCHAR(100) not null,
+  año INT not null,
+  curso VARCHAR(10) not null,
+  Area_CodAr VARCHAR(10) not null,
   FOREIGN KEY (Area_CodAr) REFERENCES Area(CodAr)
 );
 
@@ -39,22 +35,22 @@ CREATE TABLE Profesores (
 
 -- INSERTAMOS DATOS A LAS TABLAS
 
-INSERT INTO Area (CodAr, Nombre, Descripcion) VALUES
-(1, 'DAM', 'Desarrollo de Aplicaciones Multiplataforma'),
-(2, 'DAW', 'Desarrollo de Aplicaciones Web'),
-(3, 'ASIR', 'Administración de Sistemas Informáticos en Red');
+INSERT INTO Area (CodAr, Descripcion) VALUES
+('DAM', 'Desarrollo de Aplicaciones Multiplataforma'),
+('DAW', 'Desarrollo de Aplicaciones Web'),
+('ASIR', 'Administración de Sistemas Informáticos en Red');
 
-INSERT INTO Proyecto_Integrador (CodProy, nombreProy, asignatura, componentes, grupo, notaFinal, URL, año, curso, Area_CodAr) VALUES
-(1,'CodeBots', 'Base de Datos', 'Juan Cortés Cortés, Amelia María Coca López, David Rojo Villalba', 'DAM', 8.5, 'http://ejemplo.com', 2023, 1, 1),
-(2,'SmartLens', 'Programación', 'Ismael Bodas Díaz, Jorge Burgos Barrera, Javier Chicano Miguel', 'DAW', 9.2, 'http://ejemplo.com', 2023, 1, 2),
-(3,'DataSift' ,'Entornos de Desarrollo', 'Ali Chalak, Amir Mahdi Dorrani, Lucca Manfredotti García', 'ASIR', 7.8, 'http://ejemplo.com', 2023, 1, 3),
-(4, 'WebBoost','Base de Datos', 'Santiago Andres Daza Villamizar, Aris Maximilian Kuhs, David Onate Sanchez', 'DAM', 8.9, 'http://ejemplo.com', 2023, 1, 1),
-(5,'TechTonic', 'Programación', 'Daniel Garrido Nuñez, Alejandro Junyent Romani, Paula Moure Prado', 'DAW', 7.5, 'http://ejemplo.com', 2023, 1, 2),
-(6,'NetGuru' ,'Entornos de Desarrollo', 'Pablo Naranjo Cid, Gabriel Enrique Rodríguez Padrón, Celia Rubio Pais', 'ASIR', 9.0, 'http://ejemplo.com', 2023, 1, 3),
-(7,'PixelForge' ,'Base de Datos', 'Miguel Sanz Villanueva, Elena Saugar Mayoral, Alvaro Serrano Colomer', 'DAM', 6.8, 'http://ejemplo.com', 2023, 1, 1),
-(8,'CyberCore' ,'Programación', 'Josep Serrano Rayó, Wei Xu, Guoanqi Yin', 'DAW', 8.1, 'http://ejemplo.com', 2023, 1, 2),
-(9, 'InnoVent','Entornos de Desarrollo', 'James Andrew Behan, Juan Diego Motta Moncada, Mario Robles Padua', 'ASIR', 8.7, 'http://ejemplo.com', 2023, 1, 3),
-(10,'LabInfo','Programación','Aarón Juan ESCUDERO NAVAS, Daniel HERRERO MARTÍNEZ,Ismael MORENO MORAL', 'ASIR',7.8,'http://ejemplo.com', 2023, 1, 2 );
+INSERT INTO Proyecto_Integrador ( nombreProy, notaFinal, URL, año, curso, Area_CodAr) VALUES
+('CodeBots',  8.5, 'http://ejemplo.com', 2023, 1, 'DAM'),
+('SmartLens', 9.2, 'http://ejemplo.com', 2023, 1, 'DAW'),
+('DataSift' , 7.8, 'http://ejemplo.com', 2023, 1, 'ASIR'),
+('WebBoost', 8.9, 'http://ejemplo.com', 2023, 1, 'DAM'),
+('TechTonic', 7.5, 'http://ejemplo.com', 2023, 1, 'DAW'),
+('NetGuru' , 9.0, 'http://ejemplo.com', 2023, 1, 'ASIR'),
+('PixelForge' , 6.8, 'http://ejemplo.com', 2023, 1, 'DAM'),
+('CyberCore' , 8.1, 'http://ejemplo.com', 2023, 1, 'DAW'),
+('InnoVent', 8.7, 'http://ejemplo.com', 2023, 1, 'ASIR'),
+('LabInfo',7.8,'http://ejemplo.com', 2023, 1, 'DAW' );
 
 
 INSERT INTO Alumnos (codAlum,numExpediente, nombreApellidos, Proyecto_Integrador_CodProy)
